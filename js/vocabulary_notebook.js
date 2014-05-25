8965a6da518764400d4a7d9509b4d161572a5b50
QikaiGu <Qikai.Gu88@gmail.com>
2014-05-25 18:30:14 +0200

    Adds js actions

--- js/vocabulary_notebook.js
+++ js/vocabulary_notebook.js
@@ -32,10 +32,46 @@ function addWord(wordEn, wordFr, wordCn) {
     });
 }
 
-function setWordLearned() {
-
+/**
+ *
+ * @param wordId
+ */
+function setWordLearned(wordId) {
+    $.ajax({
+        type: 'POST',
+        url: 'ajaxRequest.php',
+        dateType: 'html',
+        data: {
+            event: "word_learned",
+            word_id: wordId
+        },
+        success: function(data) {
+            console.log("success: " + data);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log("error");
+        }
+    });
 }
 
-function setWordAbandoned() {
-
+/**
+ *
+ * @param wordId
+ */
+function setWordAbandoned(wordId) {
+    $.ajax({
+        type: 'POST',
+        url: 'ajaxRequest.php',
+        dateType: 'html',
+        data: {
+            event: "word_abandoned",
+            word_id: wordId
+        },
+        success: function(data) {
+            console.log("success: " + data);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log("error");
+        }
+    });
 }
